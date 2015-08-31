0704cf24d1de8c2030d28b1e217b18887e138a5a
ik <igor@kiselev.spb.ru>
2015-08-31 14:10:55 -0700

    Merge fix.

--- JSIL.Libraries/Sources/JSIL.Bootstrap.Int64.js
+++ JSIL.Libraries/Sources/JSIL.Bootstrap.Int64.js
@@ -1,14 +1,2 @@
 //? __out.GENERATE_STUBS=true;
 //? include("../Includes/Bootstrap/Int/Main.js");
-
-    $.Method({ Static: false, Public: true }, "GetHashCode",
-    (new JSIL.MethodSignature($.Int32, [], [])),
-    function UInt64_GetHashCode () {
-        return this.a | ((this.b & 0xff) << 24);
-    });
-    });
-
-    $.Method({ Static: false, Public: true }, "GetHashCode",
-    (new JSIL.MethodSignature($.Int32, [], [])),
-    function Int64_GetHashCode () {
-        return this.a | ((this.b & 0xff) << 24);
