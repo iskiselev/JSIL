9aa030ed05a8ee2f3e751507e778188948f8eafb
Igor Kiselev <igor@kiselev.spb.ru>
2016-07-21 18:59:54 -0700

    Deleted custom JSIL.Compiler and JSIL.Libraries nuspec files.

--- CreateNuGetPackage.cmd
+++ CreateNuGetPackage.cmd
@@ -1,5 +1,3 @@
 md bin\NuGet
 Upstream\NuGet Update -self
-Upstream\NuGet Pack JSIL.Compiler.nuspec -OutputDirectory bin\NuGet
-Upstream\NuGet Pack JSIL.Libraries.nuspec -OutputDirectory bin\NuGet
 Upstream\NuGet Pack JSIL.Meta.nuspec -OutputDirectory bin\NuGet
